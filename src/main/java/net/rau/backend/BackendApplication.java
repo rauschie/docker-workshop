de6519775aa5f5c057493f4437ca5434cc992f57
rau <jani.rauschenberger@hotmail.com>
2020-06-13 13:29:49 +0200

    setup basic controller

--- src/main/java/net/rau/backend/BackendApplication.java
+++ src/main/java/net/rau/backend/BackendApplication.java
@@ -2,12 +2,23 @@ package net.rau.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> greet() {
+		return ResponseEntity.ok("Hello World!");
 	}
 
 }
